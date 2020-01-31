d239e03a70b8458ac196297a6df6ca51153f7197
Joshua Taguicana <joshtaguicana@gmail.com>
2020-01-31 11:13:05 -0500

    removed mongo code from server

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 8080;
@@ -26,16 +25,6 @@ app.use("/vehicles", vehicles);
 app.use("/people", people);
 app.use("/planets", planets);
 
-// // Connects back-end with the mongoDB
-// mongoose.connect(process.env.DATABASE_URL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
-
-// const db = mongoose.connection;
-
-// db.on("error", error => console.error(error));
-// db.once("open", () => console.log("mongoDB Connected..."));
 
 
 app.listen(port, () => console.log(`We chillin on port ${port}`));
